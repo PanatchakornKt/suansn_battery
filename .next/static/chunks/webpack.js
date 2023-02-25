9d44c497e66e8ebbe1f615963a452740e321df9e
Daytech <daytech@Daytechs-MacBook-Pro.local>
2023-02-25 17:51:10 +0700

    pull main

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "23e01135c24cb27d4dc2"; }
+/******/ 		__webpack_require__.h = function() { return "9451f559dd328ae7a32e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
